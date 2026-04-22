017f6c9c212dbfe95b1a074f80e47d83c32128a1
gurvekartik6 <gurvekartik6@gmail.com>
2026-04-22 06:53:20 +0530

    First Method add

---
+++ ArrayListDemo.java
@@ -0,0 +1,9 @@
+import java.io.*;
+import java.util.*;
+public class ArrayListDemo{
+    public static void main(String []args){
+        ArrayList<String> listLanguages = new ArrayList<>();
+        //First method : add
+        listLanguages.add("C");
+    }
+}
